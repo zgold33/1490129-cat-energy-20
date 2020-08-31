aa31029287015f0e718ed174ed78a2e267099d0e
zgold33 <zgold33@gmail.com>
2020-08-31 20:19:09 +0300

    Адаптивные декоративные элементы

--- source/js/main.js
+++ source/js/main.js
@@ -1,3 +1,5 @@
+//Карта
+
 ymaps.ready(init);
 
 function init() {
@@ -25,3 +27,58 @@ function init() {
 
     map.geoObjects.add(placemark);
 }
+
+//Меню
+
+var burgerButton = document.querySelector(".page-header__toggle")
+var mainMenu = document.querySelector(".menu")
+
+burgerButton.classList.remove("page-header__toggle--nojs")
+mainMenu.classList.remove("menu--nojs")
+
+burgerButton.addEventListener("click", function () {
+  mainMenu.classList.toggle("menu--opened");
+  burgerButton.classList.toggle("page-header__toggle--inactive")
+})
+
+//Слайдер
+
+function imangeComparison() {
+    const sliderWrap = document.querySelector(".slider__wrapper");
+    const imgBefore = sliderWrap.querySelector(".slider__picture--before");
+    const imgAfter = sliderWrap.querySelector(".slider__picture--after");
+    const range = sliderWrap.querySelector("#slider");
+    const buttonBefore = sliderWrap.querySelector(".slider__button--before");
+    const buttonAfter = sliderWrap.querySelector(".slider__button--after");
+    const checkbox = sliderWrap.querySelector("slider__checkbox");
+
+    function _setImageWidth(event) {
+        imgBefore.style.width = 100 - event.currentTarget.value + "%";
+        imgAfter.style.width  = event.currentTarget.value + "%";
+    }
+
+
+function setImageClassHidden(event) {
+    if (event.currentTarget === buttonBefore || !event.currentTarget.checked) {
+        imgAfter.classlist.add("slider__picture--disables");
+        imgBefore.classlist.remove("slider__picture--disables");
+        imgAfter.style.width = 0 + "%";
+        imgBefore.style.width = 100 + "%";
+        checkbox.checked = false;
+    }
+    if (event.currentTarget === buttonBefore || !event.currentTarget.checked) {
+        imgBefore.classlist.add("slider__picture--disables");
+        imgAfter.classlist.remove("slider__picture--disables");
+        imgBefore.style.width = 0 + "%";
+        imgAfter.style.width = 100 + "%";
+        checkbox.checked = false;
+    }
+}
+
+range.addEventListener("input", _setImageWidth);
+range.addEventListener("change", _setImageWidth);
+checkbox.addEventListener("change", setImageClassHidden);
+buttonBefore.addEventListener("click", setImageClassHidden);
+buttonAfter.addEventListener("click", setImageClassHidden);
+
+window.onload = imangeComparison; }
